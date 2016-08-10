c481d439228e5086aa21bdc70b2fba09f31a7740
Zhigang Fang <zhigang1992@gmail.com>
2016-08-11 03:02:55 +0800

    Add basic example

--- App.js
+++ App.js
@@ -8,21 +8,59 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
+
+function Main({navigator}) {
+  const pushContent = ()=>navigator.push({
+    name: 'Content',
+  });
+  return (
+    <View style={styles.fullCenter}>
+      <Text style={styles.textCenter}>Main View</Text>
+      <Text style={styles.textCenter} onPress={pushContent}>Push</Text>
+    </View>
+  )
+}
+
+function Content({navigator}) {
+  const back = navigator.pop
+  return (
+    <View style={[styles.fullCenter, {backgroundColor: 'gray'}]}>
+      <Text style={styles.textCenter}>Content View</Text>
+      <Text style={styles.textCenter} onPress={back}>Back</Text>
+    </View>
+  );
+}
 
 export default class NavigatorApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-      </View>
+      <Navigator
+        style={styles.fullCenter}
+        initialRoute={{name: 'Main'}}
+        renderScene={this.renderScene}
+      />
     );
+  }
+  renderScene(route, navigator) {
+    if (route.name === 'Main') {
+      return <Main navigator={navigator}/>
+    }
+    if (route.name === 'Content') {
+      return <Content navigator={navigator}/>
+    }
   }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  fullCenter: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
-});
+  textCenter: {
+    textAlign: 'center'
+  }
+})
